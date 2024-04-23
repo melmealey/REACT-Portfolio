b204fbd96ee8d7712fb5ff0f115f2f649dec12d0
Mel Mel Mom <melmelmomof4@gmail.com>
2024-04-22 22:23:06 -0400

    readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 
 # Description
-
+I used React, React router and Vite to showcase my work as a web developer. This is a single-page application sharing about me, a few work projects, my resume and a contact form.
 
 # Intallation
 'npm install'
@@ -10,18 +10,21 @@
 'npm i @chakra-ui/react @emotion/react @emotion/styled framer-motion'
 'https://mui.com/material-ui/getting-started/installation/'
 'npm i react-router-dom'
-* Learned how to make a contact card here
-  https://www.youtube.com/watch?v=bMq2riFCF90
-  https://www.emailjs.com/docs/examples/reactjs/
-  'npm i @emailjs/browser'
+'npm i @emailjs/browser'
   
 
 # Usage
 
 
 # Credits
-
+* Learned how to make a contact card here
+  https://www.youtube.com/watch?v=bMq2riFCF90
+  https://www.emailjs.com/docs/examples/reactjs/
 
 
 # License
 Please refer to the LICENSE in the repo.
+
+# Links 
+* https://beautiful-genie-5cc59e.netlify.app/
+* https://github.com/melmealey/REACT-Portfolio
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,29 @@
 
 const Footer = () => {
-  
   return (
-    <footer style={{ backgroundColor: '#6cdae7', padding: '10px' }}>
-      <h4>Made by Mel</h4>
-      <ul>
-      <li >
-      <a href='https://github.com/melmealey'><img src="images/github Logo.webp" alt="Github" /></a>
+    <footer style={{ backgroundColor: '#6cdae7', padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <ul style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
+      <h4>Connect with Me</h4>
+        <li>
+          <a href='https://github.com/melmealey'>
+            <img src="images/github Logo.webp" alt="Github" style={{ width: '50px', height: '50px' }} />
+          </a>
         </li>
         <li>
-      <a href='https://www.linkedin.com/in/mel-mealey-bb0048a2/'><img src="images/LinkedIn logo.png" alt ="LinkedIn"/></a>
-      </li>
+          <a href='https://www.linkedin.com/in/mel-mealey-bb0048a2/'>
+            <img src="images/LinkedIn logo.png" alt="LinkedIn" style={{ width: '50px', height: '50px' }} />
+          </a>
+        </li>
+        <li>
+          <a href='https://www.facebook.com/mel.mealey'>
+            <img src="images/Facebook.svg" alt="Github" style={{ width: '50px', height: '50px' }} />
+          </a>
+        </li>
+        {/* <h4>Connect with Me</h4> */}
       </ul>
-    </footer >
-    
-  )
-  
-}
+      {/* <h4>Connect with Me</h4> */}
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
